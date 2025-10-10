d957c5d11d6c5596de9ef0ecf5f6e7e2ce205c2d
kittu0699 <kishanmishra6@gmail.com>
2025-10-11 01:13:01 +0530

    login functionality & authentication setup done !

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from './pages/Login';
+import AdminDashboard from './pages/AdminDashboard';
+import EmployeeDashboard from './pages/EmployeeDashboard';
 
 const App = () => {
   return (
-    <div className=' text-3xl text-teal-500'>WELCOME TO EMPLOYEE MANAGEMENT SYSTEM</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Navigate to="admin-dashboard" />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/admin-dashboard' element={<AdminDashboard />}></Route>
+      <Route path='/employee-dashboard' element={<EmployeeDashboard />}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App 
